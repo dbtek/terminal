f2b03f84cea66bee5bc029b1b0026568cc877178
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-03-29 14:44:23 +0200

    ui updates. billing and printing implemented.

--- app/app.js
+++ app/app.js
@@ -19,9 +19,27 @@ app.config(function ($routeProvider, $locationProvider) {
     templateUrl: 'templates/product/edit.html'
   });
 
+  $routeProvider.when('/bill/:saleId', {
+    templateUrl: 'templates/bill.html'
+  });
+
   $routeProvider.when('/browser-not-supported', {
     templateUrl: 'templates/browserNotSupported.html'
   });
 
   $routeProvider.otherwise({redirectTo: '/'});
+});
+
+app.directive('ngEnter', function() {
+  return function(scope, element, attrs) {
+    element.bind("keydown keypress", function(event) {
+      if(event.which === 13) {
+        scope.$apply(function(){
+          scope.$eval(attrs.ngEnter, {'event': event});
+        });
+
+        event.preventDefault();
+      }
+    });
+  };
 });
--- app/controllers/CashierCtrl.js
+++ app/controllers/CashierCtrl.js
@@ -1,5 +1,17 @@
-CashierCtrl = function($scope, $rootScope) {
+CashierCtrl = function($scope, $rootScope, $window, $location) {
   $scope.checkBrowser();
   $rootScope.activePage = 'cashier';
 	$scope.DAO.getProducts();
+  $scope.bill = {};
+
+  $scope.sell = function() {
+    $('#billModal').modal('hide');
+    $scope.DAO.addSale($scope.bill.product.id, $scope.bill.amount, function(results){
+      // print the bill
+      $scope.$apply(function() {
+        $location.path('/bill/'+results.insertId)
+        $location.search('print','true');
+      });
+    });
+  };
 };
